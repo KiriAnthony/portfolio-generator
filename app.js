c7e9b08ff057585737bb75cfc5aa94bc16e06722
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-02-20 11:34:07 -0700

    interfacing with inquirer package

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// "terminal.integrated.profiles.windows": {"Node.js" {"path":"C:\Program Files\nodejs", "args": []}}
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log ('Portfolio complete! Check out index.html to see the output!')
+// });
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log ('Portfolio complete! Check out index.html to see the output!')
-});
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ]).then(answers => console.log(answers));
