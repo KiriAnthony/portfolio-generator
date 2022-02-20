ed58b06076f682d87c9906aa5b4f62601c07a580
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-02-20 10:29:10 -0700

    creating an HTML file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // "terminal.integrated.profiles.windows": {"Node.js" {"path":"C:\Program Files\nodejs", "args": []}}
-const profileDataArr = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+=1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('=====================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArr);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log ('Portfolio complete! Check out index.html to see the output!')
+});
